fdec93a8318b6ad42217d24ded4bbae516d5d136
Christopher Markieta <markietachristopher@gmail.com>
2015-07-03 16:21:48 -0400

    Initial commit

---
+++ add_component.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib qw(. lib);
+
+use Bugzilla;
+use Bugzilla::Product;
+use Bugzilla::Component;
+use Bugzilla::User;
+use Bugzilla::Error;
+use Bugzilla::Constants;
+
+if (@ARGV < 2) { print "Usage: add.component.pl new_component new_summary";
+exit 1;
+}
+
+#email of bugzilla account that has admin priviledges
+my $login = 'hong.huang@senecacollege.ca';
+
+my $component_name = $ARGV[0];
+my $component_description = "";
+
+$a = 1;
+
+do {
+  $component_description = "$component_description $ARGV[$a]";
+  $a = $a + 1;
+}while ($a <= $#ARGV);
+
+print "Adding component: ", $component_name, "\n", "Description: ", $component_description, "\n";
+
+#Login to bugzilla
+my $user = new Bugzilla::User({ name => $login })
+|| ThrowUserError ('invalid_username', { name => $login });
+
+Bugzilla->set_user($user);
+
+#Get product
+my $product_name = 'LEAP';
+my $product = new Bugzilla::Product({ name => $product_name });
+
+#Create component
+my $component = Bugzilla::Component->create({
+  name => $component_name,
+  product => $product,
+  description => $component_description,
+  initialowner => 'hong.huang@senecacollege.ca'
+});
+
